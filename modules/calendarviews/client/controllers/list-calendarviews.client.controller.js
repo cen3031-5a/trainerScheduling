a4db0a1b26998f050ac1d30f4e1cc40901ec3faa
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-11-22 02:28:19 -0500

    getting it working on dev

--- modules/calendarviews/client/controllers/list-calendarviews.client.controller.js
+++ modules/calendarviews/client/controllers/list-calendarviews.client.controller.js
@@ -32,8 +32,8 @@
       if (vm.authentication.user.roles[0] === 'admin') {
         for (var i = 0; i < result.length; i++) {
           //console.log(result[i].start );
-          result[i].start = Date.parse(result[i].start).toISOString();
-          result[i].end = Date.parse(result[i].end).toISOString();
+          //result[i].start = Date.parse(result[i].start).toISOString();
+          //result[i].end = Date.parse(result[i].end).toISOString();
         }
         //console.log(vm.authentication.user.roles[0]);
         $scope.data = result;
@@ -41,8 +41,8 @@
         for(var j=0;j<result.length;j++){
           if(vm.authentication.user.username === result[j].trainer){
             //console.log(result[i].start );
-            result[j].start = Date.parse(result[j].start).toISOString();
-            result[j].end = Date.parse(result[j].end).toISOString();
+            //result[j].start = Date.parse(result[j].start).toISOString();
+            //result[j].end = Date.parse(result[j].end).toISOString();
             userOnly.push(result[j]);
           }
         }
