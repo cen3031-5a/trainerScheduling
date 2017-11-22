ec6c33e6376810593a8ebfb0caaeb2cf49568167
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-11-22 02:08:22 -0500

    adding events done except reccuring

--- modules/calendarviews/client/controllers/list-calendarviews.client.controller.js
+++ modules/calendarviews/client/controllers/list-calendarviews.client.controller.js
@@ -3,7 +3,14 @@
 
   angular.module('calendarviews').controller('CalendarviewsListController', CalendarviewsListController);
 
-  CalendarviewsListController.$inject = ['$scope','$stateParams','$state','$window', 'Authentication','CalendarviewsService'];
+  CalendarviewsListController.$inject = [
+    '$scope',
+    '$stateParams',
+    '$state',
+    '$window',
+    'Authentication',
+    'CalendarviewsService'
+  ];
 
   function CalendarviewsListController($scope, $stateParams, $state, $window, Authentication, CalendarviewsService) {
     var vm = this;
@@ -20,24 +27,29 @@
       //}
       //alert(Date.parse('11/10/2017 1:13 PM').toISOString());
 
-      console.log(vm.authentication);
+      console.log(vm.authentication.user.roles[0]);
       var userOnly = [];
-      for(var i=0;i<result.length;i++){
-        if(vm.authentication.user.username === result[i].trainer){
+      if (vm.authentication.user.roles[0] === 'admin') {
+        for (var i = 0; i < result.length; i++) {
           //console.log(result[i].start );
           result[i].start = Date.parse(result[i].start).toISOString();
           result[i].end = Date.parse(result[i].end).toISOString();
-          userOnly.push(result[i]);
         }
-      }
-      if(vm.authentication.user.roles === 'admin'){
+        //console.log(vm.authentication.user.roles[0]);
         $scope.data = result;
-      }else{
+      } else {
+        for(var j=0;j<result.length;j++){
+          if(vm.authentication.user.username === result[j].trainer){
+            //console.log(result[i].start );
+            result[j].start = Date.parse(result[j].start).toISOString();
+            result[j].end = Date.parse(result[j].end).toISOString();
+            userOnly.push(result[j]);
+          }
+        }
         $scope.data = userOnly;
       }
 
     });
-
 
   }
 }());
