844b04ef550f0deb56a8e110ce9de2f15726d597
lisbeth <lisbecg@gmail.com>
2017-11-26 19:03:02 -0500

    Changed port numbers.

--- modules/calendarviews/tests/e2e/calendarviews.e2e.tests.js
+++ modules/calendarviews/tests/e2e/calendarviews.e2e.tests.js
@@ -3,7 +3,7 @@
 describe('Calendarviews E2E Tests:', function () {
   describe('Test Calendarviews page', function () {
     it('Should report missing credentials', function () {
-      browser.get('http://localhost:3001/calendarviews');
+      browser.get('http://localhost:3000/calendarviews');
       expect(element.all(by.repeater('calendarview in calendarviews')).count()).toEqual(0);
     });
   });
